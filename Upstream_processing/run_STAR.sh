db60f9795651921caa847c5d1356a5ddf8572c33
mcadamme <mcadamme@msu.edu>
2021-01-12 05:58:15 -0500

    fixed an error with ref to sus read 2 files

--- Upstream_processing/run_STAR.sh
+++ Upstream_processing/run_STAR.sh
@@ -31,23 +31,23 @@ cat Log.final.out >> /media/megan/easystore/Roe/DGE_GenAligned_Samfiles/Log_summ
 
 
 #Susceptible
-/home/megan/src/STAR-2.7.6a/source/STAR --runThreadN 7 --genomeDir /home/megan/Genomes --readFilesCommand gunzip -c --readFilesIn /media/megan/easystore/Roe/trimmed_pairs/Roe-Suseptible_1_S1_R1_001_paired.fastq.gz /media/megan/easystore/Roe/trimmed_pairs/Roe-Suseptible_1_S1_R1_001_paired.fastq.gz --outFilterType BySJout --outFilterMultimapNmax 20 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverReadLmax 0.04 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000
+/home/megan/src/STAR-2.7.6a/source/STAR --runThreadN 7 --genomeDir /home/megan/Genomes --readFilesCommand gunzip -c --readFilesIn /media/megan/easystore/Roe/trimmed_pairs/Roe-Suseptible_1_S1_R1_001_paired.fastq.gz /media/megan/easystore/Roe/trimmed_pairs/Roe-Suseptible_1_S1_R2_001_paired.fastq.gz --outFilterType BySJout --outFilterMultimapNmax 20 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverReadLmax 0.04 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000
 
 mv Aligned.out.sam /media/megan/easystore/Roe/DGE_GenAligned_Samfiles/Sus-1_S1.sam
 cat Log.final.out >> /media/megan/easystore/Roe/DGE_GenAligned_Samfiles/Log_summary
 
-/home/megan/src/STAR-2.7.6a/source/STAR --runThreadN 7 --genomeDir /home/megan/Genomes --readFilesCommand gunzip -c --readFilesIn /media/megan/easystore/Roe/trimmed_pairs/Roe-Suseptible_2_S2_R1_001_paired.fastq.gz /media/megan/easystore/Roe/trimmed_pairs/Roe-Suseptible_2_S2_R1_001_paired.fastq.gz --outFilterType BySJout --outFilterMultimapNmax 20 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverReadLmax 0.04 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000
+/home/megan/src/STAR-2.7.6a/source/STAR --runThreadN 7 --genomeDir /home/megan/Genomes --readFilesCommand gunzip -c --readFilesIn /media/megan/easystore/Roe/trimmed_pairs/Roe-Suseptible_2_S2_R1_001_paired.fastq.gz /media/megan/easystore/Roe/trimmed_pairs/Roe-Suseptible_2_S2_R2_001_paired.fastq.gz --outFilterType BySJout --outFilterMultimapNmax 20 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverReadLmax 0.04 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000
 
 mv Aligned.out.sam /media/megan/easystore/Roe/DGE_GenAligned_Samfiles/Sus-2_S2.sam
 cat Log.final.out >> /media/megan/easystore/Roe/DGE_GenAligned_Samfiles/Log_summary
 
-/home/megan/src/STAR-2.7.6a/source/STAR --runThreadN 7 --genomeDir /home/megan/Genomes --readFilesCommand gunzip -c --readFilesIn /media/megan/easystore/Roe/trimmed_pairs/Roe-Suseptible_3_S3_R1_001_paired.fastq.gz /media/megan/easystore/Roe/trimmed_pairs/Roe-Suseptible_3_S3_R1_001_paired.fastq.gz --outFilterType BySJout --outFilterMultimapNmax 20 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverReadLmax 0.04 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000
+/home/megan/src/STAR-2.7.6a/source/STAR --runThreadN 7 --genomeDir /home/megan/Genomes --readFilesCommand gunzip -c --readFilesIn /media/megan/easystore/Roe/trimmed_pairs/Roe-Suseptible_3_S3_R1_001_paired.fastq.gz /media/megan/easystore/Roe/trimmed_pairs/Roe-Suseptible_3_S3_R2_001_paired.fastq.gz --outFilterType BySJout --outFilterMultimapNmax 20 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverReadLmax 0.04 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000
 
 mv Aligned.out.sam /media/megan/easystore/Roe/DGE_GenAligned_Samfiles/Sus-3_S3.sam
 cat Log.final.out >> /media/megan/easystore/Roe/DGE_GenAligned_Samfiles/Log_summary
 
 
-/home/megan/src/STAR-2.7.6a/source/STAR --runThreadN 7 --genomeDir /home/megan/Genomes --readFilesCommand gunzip -c --readFilesIn /media/megan/easystore/Roe/trimmed_pairs/Roe-Suseptible_4_S4_R1_001_paired.fastq.gz /media/megan/easystore/Roe/trimmed_pairs/Roe-Suseptible_4_S4_R1_001_paired.fastq.gz --outFilterType BySJout --outFilterMultimapNmax 20 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverReadLmax 0.04 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000
+/home/megan/src/STAR-2.7.6a/source/STAR --runThreadN 7 --genomeDir /home/megan/Genomes --readFilesCommand gunzip -c --readFilesIn /media/megan/easystore/Roe/trimmed_pairs/Roe-Suseptible_4_S4_R1_001_paired.fastq.gz /media/megan/easystore/Roe/trimmed_pairs/Roe-Suseptible_4_S4_R2_001_paired.fastq.gz --outFilterType BySJout --outFilterMultimapNmax 20 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverReadLmax 0.04 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000
 
 mv Aligned.out.sam /media/megan/easystore/Roe/DGE_GenAligned_Samfiles/Sus-4_S4.sam
 cat Log.final.out >> /media/megan/easystore/Roe/DGE_GenAligned_Samfiles/Log_summary
